3cdf506913b3704c0643d9070c0d0ee7f445d1ce
iamnot23 <131678378+iamnot23@users.noreply.github.com>
2026-06-17 23:25:49 +0800

    上传aiya依赖lib

--- compatibility/src/main/kotlin/top/catnies/firenchantkt/compatibility/aiyatsbus/getAviableEnachant.kt
+++ compatibility/src/main/kotlin/top/catnies/firenchantkt/compatibility/aiyatsbus/getAviableEnachant.kt
@@ -1,14 +1,11 @@
 package top.catnies.firenchantkt.compatibility.aiyatsbus
 
-import cc.polarastrum.aiyatsbus.core.AiyatsbusEnchantment
-import cc.polarastrum.aiyatsbus.module.ingame.mechanics.EnchantingTableSupport
+import cc.polarastrum.aiyatsbus.core.data.CheckType
+import cc.polarastrum.aiyatsbus.core.etsAvailable
+
 import org.bukkit.enchantments.Enchantment
 import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
-import kotlin.reflect.full.declaredMemberFunctions
-import kotlin.reflect.jvm.isAccessible
-import cc.polarastrum.aiyatsbus.core.*
-import cc.polarastrum.aiyatsbus.core.data.CheckType
 
 
 // aiya api引用转换函数
@@ -18,24 +15,24 @@ fun getAvailableEnchantments(item: ItemStack, player: Player?): List<Enchantment
         .map { it.enchantment }
 }
 
-
-val kFunction by lazy {
-    EnchantingTableSupport::class.declaredMemberFunctions.find { it.name == "doPrepareEnchant" }
-}
-
-
-// 他的这个函数会卡死
-fun getEnchantmentTableResult(player: Player, item: ItemStack, bonus: Int): List<Pair<Enchantment, Int>> {
-    return kFunction?.let { kFunction ->
-        kFunction.isAccessible = true
-        val map =
-            kFunction.call(EnchantingTableSupport, player, item, bonus) as Map<Int, Pair<AiyatsbusEnchantment, Int>>
-        val list = map.map {
-            val enchantment = it.value.first.enchantment
-            val level = it.value.second
-            enchantment to level
-        }
-        list
-    } ?: emptyList()
-
-}
+//
+//val kFunction by lazy {
+//    EnchantingTableSupport::class.declaredMemberFunctions.find { it.name == "doPrepareEnchant" }
+//}
+//
+//
+//// 他的这个函数会卡死
+//fun getEnchantmentTableResult(player: Player, item: ItemStack, bonus: Int): List<Pair<Enchantment, Int>> {
+//    return kFunction?.let { kFunction ->
+//        kFunction.isAccessible = true
+//        val map =
+//            kFunction.call(EnchantingTableSupport, player, item, bonus) as Map<Int, Pair<AiyatsbusEnchantment, Int>>
+//        val list = map.map {
+//            val enchantment = it.value.first.enchantment
+//            val level = it.value.second
+//            enchantment to level
+//        }
+//        list
+//    } ?: emptyList()
+//
+//}
